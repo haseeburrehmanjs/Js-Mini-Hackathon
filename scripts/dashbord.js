0a34a937748189c8d6a33db434ffffbf3c5424ce
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-08-16 16:07:02 +0500

    modified work

--- scripts/dashbord.js
+++ scripts/dashbord.js
@@ -1,5 +1,5 @@
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
-import { collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+import { collection, addDoc, getDocs, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 import { auth, db } from "../config.js";
 
 let title = document.querySelector('#title')
@@ -7,14 +7,21 @@ let logout_btn = document.querySelector('#logout_btn')
 let article = document.querySelector('#article')
 let form = document.querySelector('#form')
 let article_section = document.querySelector('#article_section')
+let userName = document.querySelector('#userName')
 
 let userArticle = []
+let fullName = null
 
 async function geTData() {
   const querySnapshot = await getDocs(collection(db, "articles"));
+  const querySnapshot1 = await getDocs(collection(db, "users"));
+
+  querySnapshot1.forEach((doc) => {
+    fullName = doc.data().first_name + ' ' + doc.data().last_name
+    userName.innerHTML = fullName
+  });
   querySnapshot.forEach((doc) => {
     userArticle.push(doc.data())
-    console.log(userArticle);
   });
   renderScreen()
 }
@@ -23,21 +30,19 @@ geTData()
 async function renderScreen() {
   article_section.innerHTML = ''
   userArticle.map((item, index) => {
-    console.log(item);
+    // console.log(item);
     article_section.innerHTML += `<div class="card">
   <div class="card-header">
     <div class="d-flex justify-content-between align-items-center">
-    <h1>Article</h1>
+    <h4 id="userName">${fullName}</h4>
     <h6>${item.createdAt}</h6>
     </div>
   </div>
   <div class="card-body">
-    <h5 class="card-title">${item.title}</h5>
-    <hr />
-    <p class="card-text">${item.article}</p>
+    <h2 class="card-title text-center">${item.title}</h2>
+    <p class="card-text mt-3 text-center">${item.article}</p>
   </div>
-</div>`
-  })
+</div>`})
 }
 renderScreen()
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,60 @@
 import { collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 import { auth, db } from "../config.js";
 
 let login_btn = document.querySelector('#login_btn')
 let article_section = document.querySelector('#article_section')
+let login_div = document.querySelector('#login_div')
 
 let userArticle = []
+let fullName = null
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const uid = user;
+    console.log(uid);
+    console.log('user login ha');
+    login_div.innerHTML = `<a href="./dashbord.html"><button class="btn btn-danger">DashBord</button></a>`
+  } else {
+    console.log('user login nahi ha');
+  }
+});
 
 async function geTData() {
   const querySnapshot = await getDocs(collection(db, "articles"));
+  const querySnapshot1 = await getDocs(collection(db, "users"));
+
+  querySnapshot1.forEach((doc) => {
+    fullName = doc.data().first_name + ' ' + doc.data().last_name
+    console.log(fullName);
+  })
   querySnapshot.forEach((doc) => {
-    console.log(doc.data());
     userArticle.push(doc.data())
   });
+  renderScreen()
 }
 geTData()
 
 function renderScreen() {
-  userArticle.map((item,index) => {
-  console.log(item);
+  userArticle.map((item, index) => {
+    article_section.innerHTML += `<div class="card">
+  <div class="card-header">
+    <div class="d-flex justify-content-between align-items-center">
+    <h4>${fullName}</h4>
+    <h6>${item.createdAt}</h6>
+    </div>
+  </div>
+  <div class="card-body">
+    <h2 class="card-title text-center">${item.title}</h2>
+    <p class="card-text mt-3 text-center">${item.article}</p>
+  </div>
+</div>`
   })
 }
 renderScreen()
 
 login_btn.addEventListener('click', () => {
-    window.location = './login.html'
+  window.location = './login.html'
 })
+
+
